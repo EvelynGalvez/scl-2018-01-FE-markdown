197abf2c820b501bf71d6a4b194e5c33b3162559
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-08-23 13:07:41 -0300

    esquematización de función que validará archivo para test

--- js/validateFile.js
+++ js/validateFile.js
@@ -1,1 +1,7 @@
-let allowedExtensions = /(.md)$/i;
+// let allowedExtensions = /(.md)$/i;
+
+const validateFile = () => {
+// Aquí irá función que valida que path (archivo parámetro de mdlink) corresponde a la ruta de un archivo md
+// Si es extensión md, entonces retorna true.
+// Si no es extensión md, entonces retorna false
+};
--- test/validateFile.test.js
+++ test/validateFile.test.js
@@ -1,6 +1,9 @@
 const validateFile = require('./js/validateFile');
 
-
 test('debería retornar true para archivo con extensión md', () => {
   expect(validateFile('../README.md')).toBe(true);
+});
+
+test('debería retornar false para archivo que no es md', () => {
+  expect(validateFile('ruta de archivo que no es md')).toBe(false);
 });
