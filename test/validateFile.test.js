9e3418dc5a6749fe69e32eb8011f3c6204be75f3
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-08-24 11:24:29 -0300

    3 test unitarios para 2 funciones

--- test/validateFile.test.js
+++ test/validateFile.test.js
@@ -1,9 +1,13 @@
-const validateFile = require('./js/validateFile');
+const validateFile = require('../js/validateFile');
 
-test('debería retornar true para archivo con extensión md', () => {
-  expect(validateFile('../README.md')).toBe(true);
+test('debería retornar true para path absoluto', () => {
+  expect(isAbsolut('')).toBe(true);
 });
 
-test('debería retornar false para archivo que no es md', () => {
-  expect(validateFile('ruta de archivo que no es md')).toBe(false);
+test('debería retornar true para archivo con extensión .md', () => {
+  expect(isMd('../README.md')).toBe(true);
+});
+
+test('debería retornar false para archivo con extensión distinta a .md', () => {
+  expect(isMd('ruta de archivo que no es md')).toBe(false);
 });
