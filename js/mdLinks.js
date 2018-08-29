4d6af4d168542c3b621160ea259eade84e1f2725
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-08-29 11:44:33 -0300

    commit antes de comenzar cambios para quitar 'callback hell'

--- js/mdLinks.js
+++ js/mdLinks.js
@@ -4,41 +4,53 @@ const path = require('path');
 const linkExtractor = require('./linkExtractor');
 const options = require('../index');
 
+console.log('process.argv: ' + JSON.stringify(process.argv));
+
+// Construcción de la ruta para buscar el archivo.
 const routeConstruction = () => {
-  options.validate = process.argv[3];
-  const directory = process.cwd();
-  let route = process.argv[2];
-  let absRoute = path.resolve(route);
-  console.log('ruta: ' + absRoute);
-  return absRoute;
+  if (process.argv[2] !== undefined) {
+    options.validate = process.argv[3];
+    const directory = process.cwd();
+    let route = process.argv[2];
+    let absRoute = path.resolve(route);
+    console.log('ruta: ' + absRoute);
+    let ruta = true;
+    return absRoute;
+  } else {
+    let error = 'Debes ingresar la ruta al archivo que deseas analizar, relativa a tu ubicación actual';
+    console.error(error);
+  }
+  return;
 };
 
 exports.mdLinks = (markdownLinkExtractor) => {
-  fs.readdir(routeConstruction(), (error, files) => {
-    files.forEach(file => {
-      if (path.extname(file) === '.md') {
-        console.log('se han encontrado archivos .md:');
-        console.log(file);
-        let fileMD = routeConstruction() + '\\' + file;
-        console.log(fileMD);
-        fs.readFile(fileMD, 'utf8', (err, data) => {
-          if (err) {
-            console.log(err.message);
-          } else {
-            let dataLinks = linkExtractor.markdownLinkExtractor(data);
-            dataLinks.forEach(link => {
-              fetch(link.href).then((response) => {
-                if (options.validate) {
-                  console.log(link.href + 'Status: ' + response.status + '  ' + response.statusText);
-                } else {
-                  console.log(link.href);
-                }
+  if (routeConstruction()) {
+    fs.readdir(routeConstruction(), (error, files) => {
+      files.forEach(file => {
+        if (path.extname(file) === '.md') {
+          console.log('se han encontrado archivos .md:');
+          console.log(file);
+          let fileMD = routeConstruction() + '\\' + file;
+          console.log(fileMD);
+          fs.readFile(fileMD, 'utf8', (err, data) => {
+            if (err) {
+              console.log(err.message);
+            } else {
+              let dataLinks = linkExtractor.markdownLinkExtractor(data);
+              dataLinks.forEach(link => {
+                fetch(link.href, fileMD).then((response) => {
+                  if (options.validate) {
+                    console.log(fileMD + ' ' + link.href + ' ' + 'Status: ' + response.status + '  ' + response.statusText);
+                  } else {
+                    console.log(link.href);
+                  }
+                });
               });
-            });
-          }
-        });
-      }    
+            }
+          });
+        }    
+      });
     });
-  });
-  return;
+    return;
+  };
 };
--- test/mdLinks.test.js
+++ test/mdLinks.test.js
@@ -1,13 +1,5 @@
 const mdLinks = require('../js/mdLinks');
 
-test('función routeConstruction debería retornar un valor', () => {
-  expect(directory('')).toBe(true);
-});
-
-test('debería retornar true para archivo con extensión .md', () => {
-  expect(isMd('../README.md')).toBe(true);
-});
-
-test('debería retornar false para archivo con extensión distinta a .md', () => {
-  expect(isMd('ruta de archivo que no es md')).toBe(false);
+test('esperado...', () => {
+  expect('parametro...').toBe('lo que deberia ser...');
 });
